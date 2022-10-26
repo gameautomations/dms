5c6f08e72c71c07e035a9f40fcbb0376330ad7d0
shihai5 <shihai@pandu.ie>
2022-10-26 19:10:30 +0800

    update

---
+++ dmcs/Program.cs
@@ -0,0 +1,14 @@
+﻿using dm;
+
+DmFactory.Register();
+
+var a = DmFactory.Create();
+
+Console.WriteLine(a.Ver());
+DmFactory.Release(a);
+DmFactory.Release(a);
+DmFactory.Release(a);
+DmFactory.Release(a);
+var b = DmFactory.Create();
+
+Console.WriteLine(b.Ver());
